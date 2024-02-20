416d4634726b229c38a893c39b453624bb36bdf1
David Marom <davidmarom.pro@gmail.com>
2024-02-20 17:55:25 +0200

    added GET events for test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
 "use client";
 
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Sidebar, Bar } from "@/components";
 import { FiArrowLeft } from "react-icons/fi";
-
+import { getEvents } from "@/services/events";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 console.log('aaa')
+getEvents().then(
+  (res) => { console.log(res) }
+);
+
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className="wrapper">
-        
+
         <Bar>
           <div className="rbc h100">
             <div className="rac w300px">
