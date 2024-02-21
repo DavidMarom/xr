9123a90bc2cf79aae62a62f2b1f99039b18cdc4b
David Marom <davidmarom.pro@gmail.com>
2024-02-21 17:55:23 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
   };
 
   useEffect(() => {
+    document.title = "Van Heusen";
     if (!getFromStorage("lastTransmitted")) { 
       setToStorage("lastTransmitted", new Date().getTime().toString());
     }
